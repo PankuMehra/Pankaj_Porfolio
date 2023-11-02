8d3692fea8cf2971ad9cdd550a06aae4d21d66c9
pankajmehralabs <pankaj@paraslabs.com>
2023-11-02 21:40:07 +0530

    Pro pic changed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import './app.css';
+import './App.css';
 import BackToTop from './components/helper/back-to-top/back-to-top';
 import ChangeTheme from './components/helper/change-theme/change-theme';
 import ScrollToTop from './components/helper/scroll-to-top';
--- portfolio/src/data/headerData.js
+++ portfolio/src/data/headerData.js
@@ -3,7 +3,7 @@ export const headerData = {
     name: 'Pankaj Mehra',
     title: "Hello! I am",
     desciption: "Hello! I am Pankaj Mehra, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    imagebw: 'https://i.ibb.co/2MK0cgD/Panku-filtered-picture-with-white-bg.jpg',
-    image: 'https://i.ibb.co/r4FPvKq/Panku-filtered-picture-with-black-bg.jpg',
+    imagebw: 'https://i.ibb.co/N9M9mRD/Gray-bg-pro-pic.png',
+    image: 'https://i.ibb.co/N9M9mRD/Gray-bg-pro-pic.png',
     resumePdf: 'https://drive.google.com/file/d/1Y77oCEcl7Sb-xzkQ9YYCQQ7uskeV3IV4/view?usp=sharing'
 }
